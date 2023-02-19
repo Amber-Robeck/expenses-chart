3afb1add0e1f5a3d31286cdbf3773a9fbfc04076
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2023-02-19 17:11:24 -0600

    Added js functionality to get all spans, loop through data and prepend a div to each day

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,53 @@
 console.log("Connected")
+//get all span tags from the graph
+const span = Array.from(document.querySelectorAll('.graph>span'));
+console.log(span)
+
+const data =
+    [
+        {
+            "day": "mon",
+            "amount": 17.45
+        },
+        {
+            "day": "tue",
+            "amount": 34.91
+        },
+        {
+            "day": "wed",
+            "amount": 52.36
+        },
+        {
+            "day": "thu",
+            "amount": 31.07
+        },
+        {
+            "day": "fri",
+            "amount": 23.39
+        },
+        {
+            "day": "sat",
+            "amount": 43.28
+        },
+        {
+            "day": "sun",
+            "amount": 25.48
+        }
+    ]
+
+
+span.forEach((day) =>
+    data.forEach((item) => {
+        // console.log(item.day)
+        // console.log(day.innerText)
+        if (day.innerText === item.day) {
+            console.log(day.innerText, item.amount)
+            // day.innerText = day.innerText + item.amount
+            let div = document.createElement('div');
+            div.setAttribute('class', 'bubble');
+            div.innerText = item.amount
+            day.prepend(div)
+        }
+    })
+    // console.log(day)
+)
